8a2d12a72ccc87005ea22d0761fc351b47afe6b7
denkomanceski <denkomanceski@gmail.com>
2016-05-29 12:14:49 +0200

    first commit

---
+++ routes/index.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+/* GET home page. */
+
+var path = fs.readFileSync(__dirname+'/url', 'utf-8');
+console.log(path);
+
+
+
+router.get('/', function(req, res, next) {
+
+  res.sendFile(__dirname+'/index.html');
+});
+
+router.get('/data', function(req, res){
+  var servers = {global: []};
+  var lastServerName = '';
+  var lastTotalProcessed = '';
+  var lastPerMinute = '';
+  var lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream(path)
+  });
+  lineReader.on('close', function() {
+    for(prop in servers)
+    {
+      servers[prop].reverse();
+    }
+    res.send(servers);
+  })
+  lineReader.on('line', function (line) {
+    var statisticsFor = line.indexOf("Statistics for ") > -1;
+    var totalFor = line.indexOf("Number of items matched by fingerprint: ") > -1;
+    var perMinute = line.indexOf('Number of PROCESSED items per minute') > -1;
+
+    var globalTotal = line.indexOf('Number of items matched by text: ') > -1;
+    var processedAdded = line.indexOf('Number of PROCESSED/ADDED') > -1;
+    if(statisticsFor){
+      var serverName = line.substring(line.indexOf("Statistics for ")+'Statistics for '.length, line.length-1).replace('"', '').trim().replace(" ", "_");
+      if(!servers[serverName])
+        servers[serverName] = [];
+      servers[serverName].push({name: serverName});
+      lastServerName = serverName;
+    }
+    else if(totalFor){
+      lastTotalProcessed = line.substring(line.indexOf("Total processed: ")+"Total processed: ".length, line.length);
+      servers[lastServerName][servers[lastServerName].length - 1].total = lastTotalProcessed;
+    }
+    else if(perMinute){
+      lastPerMinute = line.substring(line.indexOf("Number of PROCESSED items per minute: ")+"Number of PROCESSED items per minute: ".length, line.length);
+      servers[lastServerName][servers[lastServerName].length - 1].perMinute = lastPerMinute;
+    }
+    else if(globalTotal){
+
+      var total = parseInt(line.substring(line.indexOf("Total processed: ")+"Total processed: ".length, line.length));
+      var current = total
+      if(servers['global'].length >= 1){
+        current = total - servers['global'][servers['global'].length-1].total - 1
+      }
+      servers['global'].push({total: total, current: current})
+    }
+    else if(processedAdded){
+      var processed = line.substring(line.indexOf("Number of PROCESSED/ADDED items per minute: ")+"Number of PROCESSED/ADDED items per minute: ".length, line.length);
+      var arrs = processed.split('/');
+      servers['global'][servers['global'].length-1].processed = arrs[0];
+      servers['global'][servers['global'].length-1].added = arrs[1];
+      servers['global'][servers['global'].length-1].timestamp = Date.parse(line.substring(0, '2016-05-29 10:22:17.7137'.length));
+    }
+    console.log('Line from file:', line);
+  });
+})
+function putInfo(){
+
+}
+module.exports = router;
