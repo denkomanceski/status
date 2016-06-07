b0d1d40ff24e8c1b40456140c81f134d840b183b
Denko <denkomanceski@gmail.com>
2016-06-07 11:41:10 +0200

    as

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
+var cors = require('cors');
+app.use(cors());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
